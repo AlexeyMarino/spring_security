4833b46943a947b105ef0ced2cca5f13b0031e23
Alexey Panteleev <iMar812@yandex.ru>
2021-09-19 16:07:45 +0400

    The application configuration has been completed

---
+++ src/main/java/ru/alexeymarino/spring_security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.alexeymarino.spring_security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
