59c44ec04d9e2300584f28ae6b3eac0dae372150
Alexey Panteleev <iMar812@yandex.ru>
2021-09-19 19:11:27 +0400

    the authentication and authorization function is implemented

--- src/main/java/ru/alexeymarino/spring_security/configuration/MySecurityConfig.java
+++ src/main/java/ru/alexeymarino/spring_security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.alexeymarino.spring_security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,5 +15,12 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser(userBuilder.username("Alexey").password("Alexey").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("Elena").password("Elena").roles("HR"))
                 .withUser(userBuilder.username("Ivan").password("Ivan").roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR").antMatchers("/manager_info")
+                .hasRole("MANAGER").and().formLogin().permitAll();
     }
 }
